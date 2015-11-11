14e40e67ca167324019ba9eff95c26e753bc8c87
Jon Canning <jon.canning@gmail.com>
2015-11-11 21:45:36 +0000

    tidy up

--- src/Hornbill/Hornbill.fs
+++ src/Hornbill/Hornbill.fs
@@ -32,26 +32,35 @@ type Response =
   | StatusCode of StatusCode
   | Headers of StatusCode * Headers
   | HeadersAndBody of StatusCode * Headers * Body
-  | Raw of string list
   | Responses of Response list
   | Delegate of (Request -> Response)
   static member WithBody(statusCode, body) = Body(statusCode, body)
   static member WithStatusCode statusCode = StatusCode statusCode
   static member WithHeaders(statusCode, headers) = Headers(statusCode, headers)
   static member WithHeadersAndBody(statusCode, headers, body) = HeadersAndBody(statusCode, headers, body)
-  static member WithResponses responses = responses |> Array.toList |> Responses
-  static member WithDelegate (func : Func<Request, Response>) = Delegate (fun request -> func.Invoke request)
-  static member WithRawResponse (response : string) =
+  
+  static member WithResponses responses = 
+    responses
+    |> Array.toList
+    |> Responses
+  
+  static member WithDelegate(func : Func<Request, Response>) = Delegate(fun request -> func.Invoke request)
+  static member WithRawResponse(response : string) = 
     let lines = response.Split([| Environment.NewLine |], StringSplitOptions.None) |> Array.toList
     let (firstLine :: lines) = lines
-    let statusCode = Regex.Match(firstLine,"\s\d+\s").Value |> int
-    let rec parseHeaders headers (lines : string list) =
+    let statusCode = Regex.Match(firstLine, "\s\d+\s").Value |> int
+    
+    let rec parseHeaders headers (lines : string list) = 
       match lines with
-      | line :: lines when line.Contains ":" ->
+      | line :: lines when line.Contains ":" -> 
         let values = line.Split ':' |> Array.map (fun x -> x.Trim())
         parseHeaders ((values.[0], values.[1]) :: headers) lines
       | _ -> headers |> dict
+    
     let headers = parseHeaders [] lines
-    let body = lines |> List.skipWhile (fun x -> x <> "") |> List.reduce (+)
+    
+    let body = 
+      lines
+      |> List.skipWhile (fun x -> x <> "")
+      |> List.reduce (+)
     HeadersAndBody(statusCode, headers, body)
-
