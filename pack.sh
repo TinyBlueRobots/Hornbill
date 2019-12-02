c226e8565c32780bcf5e4808a6776afdcb9902e4
Craftyfella <craftyfella@gmail.com>
2019-12-02 16:16:03 +0000

    Increment the version number

--- pack.sh
+++ pack.sh
@@ -1,4 +1,4 @@
 export FrameworkPathOverride=$(dirname $(which mono))/../lib/mono/4.5/
-NUGETVERSION=2.4.0
+NUGETVERSION=2.5.0
 dotnet pack src/Hornbill -c Release /p:PackageVersion=$NUGETVERSION
 dotnet nuget push src/Hornbill/bin/Release/Hornbill.$NUGETVERSION.nupkg -k $NUGETKEY -s nuget.org
