ca1ccacefc719dd007e67aa01c35d6fa12533f14
Jon Canning <jon.canning@gmail.com>
2023-01-03 11:15:30 +0000

    revert to netstandard

--- pack.sh
+++ pack.sh
@@ -1,3 +1,3 @@
-NUGETVERSION=3.0.0
+NUGETVERSION=3.1.0
 dotnet pack src/Hornbill -c Release /p:PackageVersion=$NUGETVERSION
 dotnet nuget push src/Hornbill/bin/Release/Hornbill.$NUGETVERSION.nupkg -k $NUGETKEY -s nuget.org
