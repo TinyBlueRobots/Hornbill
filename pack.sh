84af042c2aaa1e643ce72f9b56339905a3c0c5f1
jon canning <jon.canning@gmail.com>
2023-10-23 13:09:58 +0100

    tidy up

--- pack.sh
+++ pack.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-NUGETVERSION=3.2.0
+NUGETVERSION=3.3.0
 dotnet pack src/Hornbill -c Release /p:PackageVersion=$NUGETVERSION
-dotnet nuget push src/Hornbill/bin/Release/Hornbill.$NUGETVERSION.nupkg -k $NUGETKEY -s nuget.org
+dotnet nuget push src/Hornbill/bin/Release/Hornbill.$NUGETVERSION.nupkg -k "$NUGETKEY" -s nuget.org
